4d66d5c706f598aa22b5e1b4e8bb57ecd1391c34
Ngoc Thach <ngocthachtruongnguyen@gmail.com>
2025-11-06 16:32:09 +0700

    Update ReplaceDamagedEV.js

--- src/App/EVMStaff/EVDelivery/ReplaceDamagedEV.js
+++ src/App/EVMStaff/EVDelivery/ReplaceDamagedEV.js
@@ -2,12 +2,12 @@ import api from "../../../api/api";
 
 export const replaceDamagedEV = async (deliveryId) => {
     try {
-        const response = await api.post(`VehicleDelivery/replace-damaged-ev/${deliveryId}`);
+        const response = await api.post(`VehicleDelivery/replace-damaged-vehicle/${deliveryId}`);
         console.log("Response data:", response.data);
         return response.data;
     }
     catch (error) {
-        console.error("Error replacing damaged EV:", error);
+        console.error("Error replacing damaged vehicle:", error);
         throw error;
     }
 }
